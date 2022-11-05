9c1abdf4e8204d696c07454e3fc4e609156b7321
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-05 13:22:20 +0000

    done

--- containdup217.py
+++ containdup217.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/contains-duplicate/
 # leetcode 217
 # easy
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        h = {}
+        for num in nums:
+            if num in h:
+                return True
+            else:
+                h[num] = 1
+        return False
