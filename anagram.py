ea71c9c547cdbf9e7331d9d01b8f4c89fdac8fb2
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-08 14:46:18 +0000

    solution 1

--- anagram.py
+++ anagram.py
@@ -1,3 +1,18 @@
 # https://leetcode.com/problems/valid-anagram/
 # leetcode 242
 # easy
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hmaps, hmapt = {}, {}
+        for i in range(len(s)):
+            hmaps[s[i]] = hmaps.get(s[i],0) + 1
+            hmapt[t[i]] = hmapt.get(t[i],0) + 1
+        
+        for ele in hmaps:
+            if hmaps[ele] != hmapt.get(ele, 0):
+                return False
+        
+        return True
