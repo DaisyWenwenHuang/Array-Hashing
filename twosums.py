abbd6f31283de59c67c4043e4f7ee04fc9b1c4c6
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-15 06:28:56 +0000

    done

--- twosums.py
+++ twosums.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/two-sum/
 # leetcode 1
 # easy
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for index,el in enumerate(nums):
+            if (target - ele) in hashmap:
+                return [hashmap[target-ele], index]
+            else:
+                hashmap[ele] = index
+                
