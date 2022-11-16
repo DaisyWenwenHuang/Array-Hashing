ed72de025e631bf07cd854e03b448626ffdedf25
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-16 17:29:06 +0000

    done

--- groupana.py
+++ groupana.py
@@ -7,4 +7,20 @@
 # what if sorted first
 # sort taking O(nlogn),and there still a outer loop 
 
-# hashmap ?
+# hashmap ? mapping each string from a-z 
+# O(m*n)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        
+        # each string in strs
+        for s in strs:
+            count = [0] * 26 # there are only 26 lowercase letters
+            
+            # each letter in string
+            for c in s:
+                count[ord(c)-ord('a')] += 1
+            res[tuple(count)].append(s)    
+            
+        return res.values()
+            
