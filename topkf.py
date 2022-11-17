f35356674fc8fbbe8c08505063cfbc0b465cf45b
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-17 17:30:56 +0000

    code

--- topkf.py
+++ topkf.py
@@ -1,3 +1,18 @@
 # https://leetcode.com/problems/top-k-frequent-elements/
 # leetcode 347
 # medium
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        f = [[] for i in range(len(nums) + 1)]
+        for ele in nums:
+            hashmap[ele] = hashmap.get(ele,0) + 1
+        for n , c in hashmap.items():
+            f[c].append(n)
+        
+        res = []
+        for i in range(len(f)-1,0,-1):
+            for n in f[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
